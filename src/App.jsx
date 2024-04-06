9e10d9a0c785e159ff5180fddbbc8b92b7341b9e
Emediong Bassey Edem <edememediong1@gmail.com>
2024-04-07 00:00:02 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import {useState} from 'react';
 import Player from "./components/PlayersInfo"
 import GameBoard from "./components/GameBoard"
+import Log from './components/Log'
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([])
   const [activePlayer, setActivePlayer] = useState('X')
   
   function handleSelectSquare(){
     setActivePlayer((curActivePlayer) => curActivePlayer == 'X'? "O" : "X");
+    setGameTurns()
   }
   return (
     <main>
